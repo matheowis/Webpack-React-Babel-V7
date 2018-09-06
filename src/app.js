6ec35d421470de5079fb94a14d95775e7bab4add
Wiśniowski Mateusz <mateuszwis93@gmail.com>
2018-09-06 08:36:21 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const DashboardPage = () => (
+  <h1>Hello World!</h1>
+)
+
+const jsx = (
+		<DashboardPage />
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
